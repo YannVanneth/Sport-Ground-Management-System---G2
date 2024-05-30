2bbd7dbf9d5d6681f206a76c9930073415e514f2
Vanneth Yann <yannvanneth.27@gmail.com>
2024-05-30 10:04:44 +0700

    add Design class

---
+++ Source-code/Header/Project_Header/Design/storeConsole.h
@@ -0,0 +1,20 @@
+#ifndef __STORECONSOLE_H__
+#define __STORECONSOLE_H__
+
+//--------------------------------------------------------------------------------------//
+// User Define Header
+#include "../../DNN_Header/DNN.h"
+//--------------------------------------------------------------------------------------//
+
+//////////////////////////////////////////
+//    DESIGN STORE CLASS DEFINITION     //
+/////////////////////////////////////////
+
+namespace DNN{
+    class Design::Store{
+        public:
+           // Statements 
+    };
+}
+
+#endif
