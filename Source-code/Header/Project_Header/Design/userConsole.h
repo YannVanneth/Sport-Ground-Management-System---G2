b6aeaf6c8f04dc3b6bc31ec279c70fca8e069c14
Vanneth Yann <yannvanneth.27@gmail.com>
2024-05-30 10:04:44 +0700

    add Design class

---
+++ Source-code/Header/Project_Header/Design/userConsole.h
@@ -0,0 +1,20 @@
+#ifndef __USERCONSOLE_H__
+#define __USERCONSOLE_H__
+
+//--------------------------------------------------------------------------------------//
+// User Define Header
+#include "../../DNN_Header/DNN.h"
+//--------------------------------------------------------------------------------------//
+
+//////////////////////////////////////////
+//    DESIGN USER CLASS DEFINITION      //
+/////////////////////////////////////////
+
+namespace DNN{
+    class Design::User{
+        public:
+           // Statements 
+    };
+}
+
+#endif
