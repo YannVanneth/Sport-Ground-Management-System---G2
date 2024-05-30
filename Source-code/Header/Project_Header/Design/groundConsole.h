3b997fd59e63f3299f6ecaa511119f2c271af786
Vanneth Yann <yannvanneth.27@gmail.com>
2024-05-30 10:04:44 +0700

    add Design class

---
+++ Source-code/Header/Project_Header/Design/groundConsole.h
@@ -0,0 +1,20 @@
+#ifndef __GROUNDCONSOLE_H__
+#define __GROUNDCONSOLE_H__
+
+//--------------------------------------------------------------------------------------//
+// User Define Header
+#include "../../DNN_Header/DNN.h"
+//--------------------------------------------------------------------------------------//
+
+//////////////////////////////////////////
+//    DESIGN GROUND CLASS DEFINITION    //
+/////////////////////////////////////////
+
+namespace DNN{
+    class Design::Ground{
+        public:
+           // Statements 
+    };
+}
+
+#endif
